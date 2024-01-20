d9531213ab250a9a061b70128c47b38d30a00f9d
Fahmi Zain <fahmizain000@gmail.com>
2024-01-20 20:32:54 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-center font-semibold text-3xl'>Coming Soon...</h1>
+    </div>
+  )
+}
+
+export default App
