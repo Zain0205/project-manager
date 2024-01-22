3f9e63a5ba0e9a1f4ceb28fa34cfe99d9a19b43e
Fahmi Zain <fahmizain000@gmail.com>
2024-01-22 17:00:21 +0700

    feat: adding card component template

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <>
+     <div className="w-[90%] h-60 mx-auto my-5 px-5 shadow-md">
+      addasdsad
+     </div> 
+    </>
+  )
+}
+
+export default Card
