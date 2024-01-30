289cc8db08708ede3029c2a612e39e2fb56d0303
Fahmi Zain <fahmizain000@gmail.com>
2024-01-30 20:32:50 +0700

    fix: fixing styling

--- src/components/NotSelected.jsx
+++ src/components/NotSelected.jsx
@@ -1,10 +1,12 @@
 import Button from "./Button.jsx";
 
-export default function NotSelected({onAdd}){
+export default function NotSelected({ onAdd }) {
   return (
     <>
-      <h1>NO PROJECT SELECTED</h1>
-      <Button onClick={onAdd} >Add</Button>
+      <div className="text-center lg:text-left">
+        <h1 className="font-bold text-xl mb-3">NO PROJECT SELECTED</h1>
+        <button onClick={onAdd} className="bg-stone-900 text-slate-100 px-10 py-2 rounded-full">Add</button>
+      </div>
     </>
-  )
+  );
 }
