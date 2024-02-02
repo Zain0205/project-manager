bf9452005c5124f454a6d84e6cebdbd568a02f07
Fahmi Zain <fahmizain000@gmail.com>
2024-02-02 09:03:09 +0700

    useContext hooks practice

--- src/components/NotSelected.jsx
+++ src/components/NotSelected.jsx
@@ -1,11 +1,16 @@
-import Button from "./Button.jsx";
+import { useContext } from "react";
+import { StateContext } from "../store/context";
 
-export default function NotSelected({ onAdd }) {
+export default function NotSelected() {
+  const { addNewProjectTab } = useContext(StateContext);
+
   return (
     <>
       <div className="text-center lg:text-left">
         <h1 className="font-bold text-xl mb-3">NO PROJECT SELECTED</h1>
-        <button onClick={onAdd} className="bg-stone-900 text-slate-100 px-10 py-2 rounded-full">Add</button>
+        <button onClick={addNewProjectTab} className="bg-stone-900 text-slate-100 px-10 py-2 rounded-full">
+          Add
+        </button>
       </div>
     </>
   );
