8fff11a6d4dadc86082c54fae17bde11ed65e68c
Fahmi Zain <fahmizain000@gmail.com>
2024-02-05 09:02:49 +0700

    cleaning the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,12 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import SideBar from "./components/SideBar.jsx";
-import NewProjectInput from "./components/NewProjectInput.jsx";
-import NotSelected from "./components/NotSelected.jsx";
-import SelectedProject from "./components/SelectedProject.jsx";
+import ContextProvider from "./store/context.jsx";
 
 function App() {
-  const [projectManager, setProjectManager] = useState({
-    selectedTab: undefined,
-    projects: [],
-    task: [],
-  });
-
-  const handleDellete = () => {
-    setProjectManager((prev) => {
-      return {
-        ...prev,
-        selectedTab: null,
-        projects: prev.projects.filter((project) => project.id !== prev.selectedTab),
-      };
-    });
-  };
-
-  const handleSelectProject = (id) => {
-    setProjectManager((prev) => {
-      return {
-        ...prev,
-        selectedTab: id,
-      };
-    });
-  };
-
-  const handleAddNewProjectClick = (newProject) => {
-    setProjectManager((prev) => {
-      const newProjectData = {
-        ...newProject,
-        id: Math.random(),
-      };
-
-      return {
-        ...prev,
-        selectedTab: undefined,
-        projects: [...prev.projects, newProjectData],
-      };
-    });
-  };
-
-  const handleNewPoject = () => {
-    setProjectManager((prev) => {
-      return {
-        ...prev,
-        selectedTab: null,
-      };
-    });
-  };
-
-  const handleCancelAddProject = () => {
-    setProjectManager((prev) => {
-      return {
-        ...prev,
-        selectedTab: undefined,
-      };
-    });
-  };
-
-  const selectProject = projectManager.projects.find((project) => project.id === projectManager.selectedTab);
-
-  let content = <SelectedProject onDellete={handleDellete} project={selectProject} />;
-
-  if (projectManager.selectedTab === undefined) {
-    content = <NotSelected onAdd={handleNewPoject} />;
-  } else if (projectManager.selectedTab === null) {
-    content = <NewProjectInput cancelProject={handleCancelAddProject} addProject={handleAddNewProjectClick} />;
-  }
-
-  // projectManager.selectedTab ? (content = <NotSelected onAdd={handleNewPoject} />) : (content = <NewProjectInput cancelProject={handleCancelAddProject} addProject={handleAddNewProjectClick} />);
-
   return (
-    <>
-      <SideBar onSelect={handleSelectProject} projectList={projectManager.projects} onAdd={handleNewPoject} />
-      <main className="pt-24 px-5 md:pl-[21rem]">{content}</main>
-    </>
+    <ContextProvider>
+      <SideBar />
+    </ContextProvider>
   );
 }
 
